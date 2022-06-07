21d2dc6a52c9f7a648099e8b26462f8975c836ff
kuroyukinesha <kuroyukinesha@gmail.com>
2022-06-07 23:15:54 +0700

    first commit

---
+++ app/src/main/java/com/example/uts_oji_10119111/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.uts_oji_10119111;
+//nim : 10119111
+//nama : fauzi fikrinullah
+//kelas : if-3
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
